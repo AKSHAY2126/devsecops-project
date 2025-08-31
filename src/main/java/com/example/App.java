0737b14d4131956cd7776a8b37d33042783ff3d0
AKSHAY2126 <akshaysingh5000@gmail.com>
2025-08-31 14:55:28 +0530

    workflow update

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello SonarQube!");
+    }
+
+    // A simple method to trigger sonar analysis
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
